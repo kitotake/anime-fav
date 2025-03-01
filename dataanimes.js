9504a692cf750a4d492132068052478490885cf1
kitotake <76629813+kitotake@users.noreply.github.com>
2025-03-01 17:52:48 +0100

    add html, css and js coding

---
+++ dataanimes.js
@@ -0,0 +1,44 @@
+const animeData = [
+    {
+        titre: "Jujutsu Kaisen",
+        annee: "2020–",
+        genre: "Animation, Action, Aventure",
+        type: "série",
+        saisons: "2",
+        poster: "https://m.media-amazon.com/images/M/MV5BNGY4MTg3NzgtMmFkZi00NTg5LWExMmEtMWI3YzI1ODdmMWQ1XkEyXkFqcGdeQXVyMjQwMDg0Ng@@._V1_SX300.jpg"
+    },
+    {
+        titre: "Naruto",
+        annee: "2002–2007",
+        genre: "Animation, Action, Aventure",
+        type: "série",
+        saisons: "1",
+        poster: "https://m.media-amazon.com/images/M/MV5BZmQ5NGFiNWEtMmMyMC00MDdiLTg4YjktOGY5Yzc2MDUxMTE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+    },
+    {
+        titre: "Dragon Ball",
+        annee: "1986–1989",
+        genre: "Animation, Action, Aventure",
+        type: "série",
+        saisons: "1",
+        poster: "https://m.media-amazon.com/images/M/MV5BYzI0YjYxY2UtNzRjNS00NTZiLTgzMDItNGEzMjU5MmE0ZWJmXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_SX300.jpg"
+    },
+    {
+        titre: "Dragon Ball Z",
+        annee: "1996–2003",
+        genre: "Animation, Action, Aventure",
+        type: "série",
+        saisons: "16",
+        poster: "https://m.media-amazon.com/images/M/MV5BNGM5MTEyZDItZWNhOS00NzNkLTgwZTAtNWIzY2IzZmExOWMxXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+    },
+    {
+        titre: "Shangri-La Frontier",
+        annee: "2023–2024",
+        genre: "Animation, Action, Aventure",
+        type: "série",
+        saisons: "1",
+        poster: "https://m.media-amazon.com/images/M/MV5BMjNmNzU4MDMtOGU2ZS00YzIwLWJhMmYtMGQ3MGE0YTA2M2NjXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_SX300.jpg"
+    }  
+];
+
+document.addEventListener('DOMContentLoaded', displayAnime);
